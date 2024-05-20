4ffb4d400795a7603b8baf9663b8aa1000b60f79
Rudra Patel <rudrapatel26122004@gmail.com>
2024-05-19 22:52:30 -0400

    fixes, and more issues

--- util/util.py
+++ util/util.py
@@ -4,13 +4,12 @@ import yaml
 from torchvision import transforms as t
 
 config = yaml.safe_load(open('constants/v1.yaml'))
-classes = np.array(config['Dataset']['classes'])
+classes = np.array([item for item in config['Dataset']['classes'] if item not in config['Dataset']['redactions']])
 
 
 # given a torch array, return the index of the top 5 logits
-def pick_top(x: torch.Tensor, count=1, sorted: bool = True):
-    return x.topk(k=count, sorted=sorted)
-
+def pick_top(x: torch.Tensor, count=1, sorted: bool = True, prettify: bool = False):
+    return x.topk(k=count, sorted=sorted) if not prettify else prettify_classes(x.topk(k=count, sorted=sorted))
 
 # return the parsed input
 def prettify_classes(x):
@@ -26,14 +25,14 @@ to_pil = t.ToPILImage()
 
 
 @torch.no_grad()
-def calculate_precession(y_hat: torch.Tensor, y: torch.Tensor, pooling = False, cutoff=.5):
+def calculate_precession(y_hat: torch.Tensor, y: torch.Tensor, pooling = False, cutoff=.35):
     y = y.cpu().clone()
     y_hat = y_hat.cpu().clone()
 
-    classes_hat = y_hat.clone().apply_(lambda x: 1 if x > .35 else 0)
+    classes_hat = y_hat.clone().apply_(lambda x: 1 if x > cutoff else 0)
     y_complement = y.clone().apply_(lambda x: 0 if x == 1 else 1)
 
-    acc = ((classes_hat * y).sum(dim = -1) - (y_complement * classes_hat).sum(dim=-1))
+    acc = (((classes_hat * y).sum(dim = -1) - (y_complement * classes_hat).sum(dim=-1))) / torch.sum(y, dim=-1)
 
     return torch.mean(acc) if pooling else acc
 
